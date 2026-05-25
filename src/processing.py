4c2b0153588db376a715d8cad0a29cb9f61a58b0
LeshkaKlein <leshkaklein@gmail.com>
2026-05-25 15:16:57 +0500

    style: remove transliteration

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-def filter_by_state(my_list_dict: list, state: str = "EXECUTED") -> list:
+def filter_by_state(my_list_state: list, state: str = "EXECUTED") -> list:
     """Фильтрует список словарей, оставляя только записи с заданным статусом."""
 
-    return [items for items in my_list_dict if items.get("state") == state]
+    return [items for items in my_list_state if items.get("state") == state]
 
 
-def sort_by_date(my_list_dir: list, descending: bool = True) -> list:
+def sort_by_date(my_list_date: list, descending: bool = True) -> list:
     """Сортирует список словарей по дате (по умолчанию — от новых к старым)."""
-    sorted_list = sorted(my_list_dir, key=lambda employee: employee["date"], reverse=descending)
+    sorted_list = sorted(my_list_date, key=lambda employee: employee["date"], reverse=descending)
 
     return sorted_list
